1a4a05fa3a5abb5c9acad12b7ae980e5c467ca0e
SharmaSonali1997 <43983188+SharmaSonali1997@users.noreply.github.com>
2019-02-26 21:44:46 +0530

    Update samp.c

--- samp.c
+++ samp.c
@@ -2,7 +2,7 @@
 int main() 
 { 
     int arr[] = {1,2,3,4,5}; 
-    printf("arr [0] is %d\n", arr[0]);      
+    printf("Sonali Sharma");      
     
     printf("arr[10] is %d\n", arr[10]); 
     return 0; 
